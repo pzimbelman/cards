1341fe471a11ad1ee5a0fad5364089c6f2cfa04a
Pete <pzimbelman@gmail.com>
2010-12-13 00:47:24 -0800

    removing duplicate ace to five helper check

--- lib/straight_comparisons.rb
+++ lib/straight_comparisons.rb
@@ -1,17 +1,11 @@
 module Game
   module StraightComparisons
-    protected
-    def is_ace_to_five?
-      if high_card.rank == "A" && @cards[1].rank == 5
-        return true
-      end
-    end
 
     private
     def compare_same_rank(opponent)
-      if is_ace_to_five?
+      if is_ace_to_five?(cards)
         return false
-      elsif opponent.is_ace_to_five?
+      elsif is_ace_to_five?(opponent.cards)
         return true
       end
       return self.high_card > opponent.high_card
